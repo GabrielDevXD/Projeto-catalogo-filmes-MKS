affdf56d8e1580f069d36ac16d84d1ddfe456eb6
GabrielDevXD <tantantaon1@gmail.com>
2022-07-10 20:19:41 -0300

    DtoCreate (Integration Dto create)

--- src/filmes/dto/create-filme.dto.ts
+++ src/filmes/dto/create-filme.dto.ts
@@ -1,3 +1,35 @@
+import { ApiProperty } from "@nestjs/swagger";
+import { IsNumber, IsPositive, IsString, IsUrl } from 'class-validator';
 
-
-export class CreateFilmeDto {}
+export class CreateFilmeDto {
+    @ApiProperty({ description: 'Nome do filme',
+     example: 'Avangers the end' })
+    @IsString()
+    title: string;
+    @ApiProperty({
+      description: 'Image do filme',
+      example: 'LinkDaImagem',
+    })
+    @IsString()
+    coverImageUrl: string;
+    @IsString()
+    @ApiProperty({
+      description: 'Descrição do filme',
+      example: 'After Thanos, an intergalactic warlord, disintegrates half of the universe, the Avengers must reunite and assemble again to reinvigorate their trounced allies and restore balance.',
+    })
+    description: string;
+    @ApiProperty({ description: 'ano do jogo', example: 2019})
+    @IsNumber()
+    @IsPositive()
+    year: number;
+    @ApiProperty({ description: 'Nota do game', example: 8})
+    @IsNumber()
+    @IsPositive()
+    imdbScore: number;
+    @IsUrl()
+    @ApiProperty({
+      description: 'trailer do filme',
+      example: 'Link do trailer',
+    })
+    trailerYoutubeUrl: string; 
+  }
