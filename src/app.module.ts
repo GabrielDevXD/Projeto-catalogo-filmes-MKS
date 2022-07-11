280895c580371803ace03c3d30efe650c76543d1
GabrielDevXD <tantantaon1@gmail.com>
2022-07-10 21:13:11 -0300

    Schema update + prisma service,module

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,11 +8,12 @@ import { ProfileModule } from './profile/profile.module';
 import { HomepageModule } from './homepage/homepage.module';
 import { UserModule } from './user/user.module';
 import { PrismaService } from './prisma/prisma.service';
+import { PrismaModule } from './prisma/prisma.module';
 
 
 @Module({
   controllers: [AppController],
   providers: [AppService, PrismaService],
-  imports: [FilmesModule, AuthModule, GenreModule, ProfileModule, HomepageModule, UserModule],
+  imports: [FilmesModule, AuthModule, GenreModule, ProfileModule, HomepageModule, UserModule, PrismaModule],
 })
 export class AppModule {}
